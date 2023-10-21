a0a4bf79b00a471e1b654e904affefab1a84137b
galthran-wq <levmorozov900@gmail.com>
2023-10-21 15:14:14 +0000

    change collate

--- facade_datasets/utils.py
+++ facade_datasets/utils.py
@@ -3,10 +3,10 @@ import torch
 
 def collate_fn(batch):
     pixel_values = torch.stack([example["pixel_values"] for example in batch])
-    pixel_mask = torch.stack([example["pixel_mask"] for example in batch])
-    class_labels = ([example["class_labels"] for example in batch])
-    mask_labels = torch.stack([example["mask_labels"] for example in batch])
-    return {"pixel_values": pixel_values, "pixel_mask": pixel_mask, "class_labels": class_labels, "mask_labels": mask_labels}
+    # pixel_mask = torch.stack([example["pixel_mask"] for example in batch])
+    # class_labels = ([example["class_labels"] for example in batch])
+    mask_labels = torch.stack([example["labels"] for example in batch])
+    return {"pixel_values": pixel_values, "labels": mask_labels}
 
 
 def get_dataloader(df, *args, **kwargs):
