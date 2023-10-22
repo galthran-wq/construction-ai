dfb9c107ad8925441223a71cd5acf7c230f81de6
galthran-wq <levmorozov900@gmail.com>
2023-10-22 09:55:04 +0000

    viz eval for seg

--- visualize_eval.py
+++ visualize_eval.py
@@ -1,5 +1,7 @@
 import os
 import argparse
+
+import torch.nn as nn
 import numpy as np
 import torch
 from collections import defaultdict
@@ -8,10 +10,9 @@ import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 from PIL import Image
 import albumentations as A
-from transformers import MaskFormerImageProcessor, MaskFormerForInstanceSegmentation
+from transformers import AutoImageProcessor, AutoModelForSemanticSegmentation
 
-from facade_datasets.const import id2class_general
-from facade_datasets.etrims.const import images_base_path as cmp_images_base_path
+from train_copy import get_dataset
 
 
 def build_args():
@@ -20,8 +21,10 @@ def build_args():
                     description='What the program does',
                     epilog='Text at the bottom of help')
     parser.add_argument('-m', '--model', default=None)      
-    parser.add_argument('-i', '--input-path', default=cmp_images_base_path / os.listdir(cmp_images_base_path)[0])      
+    parser.add_argument('-p', '--preprocessor', default=None)      
+    parser.add_argument('-d', '--dataset')      
     parser.add_argument('-o', '--output-path', default="./segmentation_example")      
+    parser.add_argument('-idx', default=0, type=int)      
     return parser
 
 
@@ -48,65 +51,62 @@ def draw_panoptic_segmentation(segmentation, segments, model, ax):
     return fig, axs
 
 
-def preprocess_image(image, type="etrims"):
-    # note that you can include more fancy data augmentation methods here
-    
-    etrims_mean , etrims_std = torch.tensor([0.0017, 0.0017, 0.0017]), torch.tensor([0.0011, 0.0011, 0.0012])
-    cmp_mean , cmp_std = torch.tensor([0.0019, 0.0018, 0.0016]), torch.tensor([0.0010, 0.0009, 0.0009])
-    cars_mean, cars_std = torch.tensor([0.0021, 0.0020, 0.0023]), torch.tensor([0.0012, 0.0012, 0.0012])
-
-    if type == "etrims":
-        mean, std = etrims_mean, etrims_std
-    elif type == "cmp":
-        mean, std = cmp_mean, cmp_std
-    elif type == "cars":
-        mean, std = cars_mean, cars_std
-
-    train_transform = A.Compose([
-        A.Resize(width=512, height=512),
-        A.Normalize(mean=mean, std=std),
-    ])
-    processor = MaskFormerImageProcessor(do_resize=False, do_rescale=False, do_normalize=False, ignore_index=0)
+def preprocess_image(image):
     image_arr = np.array(image)
     transformed_image = train_transform(image=image_arr)["image"]
-    inputs = processor(images=transformed_image, return_tensors="pt")
+    inputs = processor(images=[transformed_image], return_tensors="pt")
     return inputs 
 
 
 def eval(model, inputs):
     if model is not None:
-        model = MaskFormerForInstanceSegmentation.from_pretrained(model)
-    else: # if you want to compare to before-uptrain result
-        model = MaskFormerForInstanceSegmentation.from_pretrained("facebook/maskformer-swin-large-coco",
-                                                          id2label=id2class_general,
-                                                          ignore_mismatched_sizes=True)
-    outputs = model(**inputs)
+        model = AutoModelForSemanticSegmentation.from_pretrained(model)
+    # else: # if you want to compare to before-uptrain result
+    #     model = MaskFormerForInstanceSegmentation.from_pretrained("facebook/maskformer-swin-large-coco",
+    #                                                       id2label=id2class_general,
+    #                                                       ignore_mismatched_sizes=True)
+    print(inputs.keys())
+    labels = inputs['labels']
+    # inputs['pixel_mask'] = inputs['pixel_mask']
+    outputs = model(pixel_values=inputs['pixel_values'][None, :, :, :])
+    upsampled_logits = nn.functional.interpolate(
+        outputs.logits,
+        size=labels.shape[::-1],
+        mode="bilinear",
+        align_corners=False,
+    )
+    result = upsampled_logits.argmax(dim=1)[0]
+
     # model predicts class_queries_logits of shape `(batch_size, num_queries)`
     # and masks_queries_logits of shape `(batch_size, num_queries, height, width)`
     # class_queries_logits = outputs.class_queries_logits
     # masks_queries_logits = outputs.masks_queries_logits
 
-    # you can pass them to image_processor for postprocessing
-    processor = MaskFormerImageProcessor(do_resize=False, do_rescale=False, do_normalize=False, ignore_index=0)
-    result = processor.post_process_panoptic_segmentation(outputs, target_sizes=[image.size[::-1]])[0]
-    return model, result
+    return result
     
 # def main(input_path, output_path, model)
 
 
 if __name__ == "__main__":
     args = build_args().parse_args()
-    image = Image.open(args.input_path)
-    if "cmp" in str(args.input_path):
-        type = "cmp"
-    elif "cars" in str(args.input_path):
-        type = "cars"
-    elif "etrims" in str(args.input_path):
-        type = "etrims"
-    preprocessed_image = preprocess_image(image, type=type)
-    model, result = eval(args.model, preprocessed_image)
-    fig, axs = plt.subplots(1, 2, figsize=(15, 10))
-    draw_panoptic_segmentation(result['segmentation'], result['segments_info'], model = model, ax = axs[1])
-    axs[0].imshow(image)
+    train_transform = A.Compose([
+        A.Resize(width=256, height=256),
+        A.Normalize(mean=[0,0,0], std=[1,1,1]),
+    ])
+    processor = AutoImageProcessor.from_pretrained(
+        args.preprocessor, do_resize=False, do_rescale=False, do_normalize=False, ignore_index=0
+    )
+    Dataset = get_dataset(args.dataset)
+    dataset = Dataset(transform=train_transform, processor=processor)
+    idx = args.idx
+    inputs = dataset[idx]
+    result = eval(args.model, inputs)
+    fig, axs = plt.subplots(1, 3, figsize=(15, 10))
+    axs[0].imshow(dataset.open_as_array(idx))
+    axs[1].imshow(result)
+    axs[2].imshow(inputs['labels'])
+    axs[0].set_title("original image")
+    axs[1].set_title("predicted segmentation")
+    axs[2].set_title("true segmentation")
     fig.savefig(args.output_path)
 
